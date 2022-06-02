9c6e127bb942661fda66783b340e7d2ca869770a
dmaurya98 <deepakmaurya9588@gmail.com>
2022-06-02 10:55:49 +0530

    reduxapp

---
+++ src/component/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className="container">
+      <h2> Deposit/Withdraw Money</h2>
+      <button className="btn btn-primary mx-2">-</button>
+      Update Balance
+      <button className="btn btn-primary mx-2">+</button>
+
+    </div>
+  )
+}
+
+export default Shop
